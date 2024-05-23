b526dc1f754b4b9fcad8ff5b0a95ea7310687833
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-05-23 08:06:00 +0300

    Add files via upload

---
+++ L01_Data_types_and_tables/Lab/07_insert_data_in_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees
+(first_name,middle_name, last_name, hiring_date, salary, devices_number)
+VALUES
+('Peter', 'Ivanov', 'Ivanov', '2023-01-01', 1000, 1),
+('Todor', 'Ivanov', 'Ivanov', '2023-01-01', 1000, 1),
+('Bob', 'Petrov', 'Petrov', '2023-01-01', 1000, 1);
