049d9abd7a9d8a555d4c754320b5b45ff181093e
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-06-07 13:19:32 +0300

    Add files via upload

---
+++ L01_Data_types_and_tables/Exercise/14_select.sql
@@ -0,0 +1,2 @@
+SELECT name, task, email, banana FROM minions_info;
+-- fetch information from the "minions_info" table. Therefore, he has approached you to write a SQL query that can retrieve the "name", "task", "email", and "banana" columns for him.
