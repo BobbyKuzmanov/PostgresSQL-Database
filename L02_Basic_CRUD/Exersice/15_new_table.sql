01edfd2bd6308b1d80307e6c155711251da9d485
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-07-01 07:54:01 +0300

    Add files via upload

---
+++ L02_Basic_CRUD/Exersice/15_new_table.sql
@@ -0,0 +1,9 @@
+CREATE VIEW company_chart AS
+SELECT
+    CONCAT(first_name, ' ', last_name) AS full_name,
+   job_title,
+   department_id,
+   manager_id
+FROM employees;
+
+SELECT * FROM company_chart;
