595a51a00bcff982358abd9505b3ced47e91af91
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-11-25 05:34:04 +0200

    Add files via upload

---
+++ L08_Exam_exersices/Exam_2/09_full_info_of_courses.sql
@@ -0,0 +1,23 @@
+SELECT
+    a.name AS address,
+    CASE
+        WHEN EXTRACT(HOUR FROM co.start) BETWEEN 6 AND 20 THEN 'Day'
+        ELSE 'Night'
+    END AS day_time,
+    co.bill,
+    c.full_name,
+    ca.make,
+    ca.model,
+    cat.name AS category_name
+FROM
+    courses co
+JOIN
+    addresses a ON co.from_address_id = a.id
+JOIN
+    clients c ON co.client_id = c.id
+JOIN
+    cars ca ON co.car_id = ca.id
+JOIN
+    categories cat ON ca.category_id = cat.id
+ORDER BY
+    co.id;
