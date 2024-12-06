9aab680700fc56079eb357c359489ff61fefa402
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-12-06 04:52:06 +0200

    Add files via upload

---
+++ L08_Exam_exersices/Exam_3/09_creators_and_emails.sql
@@ -0,0 +1,16 @@
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+    c.email,
+    MAX(bg.rating) AS rating
+FROM
+    creators c
+JOIN
+    creators_board_games cbg ON c.id = cbg.creator_id
+JOIN
+    board_games bg ON cbg.board_game_id = bg.id
+WHERE
+    c.email LIKE '%.com'
+GROUP BY
+    c.id
+ORDER BY
+    full_name ASC;
