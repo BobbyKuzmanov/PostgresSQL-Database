f21fab4e4a61f2d220e74978e951c90c1e978c14
BobbyKuzmanov <7870699+BobbyKuzmanov@users.noreply.github.com>
2024-12-19 05:05:26 +0200

    Add files via upload

---
+++ L08_Exam_exersices/Exam_4_14 October 2023/09_stadium_teams_info.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION fn_stadium_team_name(stadium_name VARCHAR(30))
+RETURNS TABLE (team_name VARCHAR(45)) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT t.name
+    FROM teams t
+    JOIN stadiums s ON t.stadium_id = s.id
+    WHERE s.name = stadium_name
+    ORDER BY t.name ASC;
+END;
+$$ LANGUAGE plpgsql;
+
+
+--test
+SELECT fn_stadium_team_name('BlogXS');
+SELECT fn_stadium_team_name('Quaxo');
+SELECT fn_stadium_team_name('Jaxworks')
